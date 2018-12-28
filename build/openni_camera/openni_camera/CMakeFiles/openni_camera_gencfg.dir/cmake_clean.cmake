e3035b16f00dd0c5603e7e0e2f4340b2a297e248
Sinchiguano <cesarsinchiguano@hotmail.es>
2018-12-28 12:11:43 +0100

    first commit

---
+++ build/openni_camera/openni_camera/CMakeFiles/openni_camera_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openni_camera_gencfg"
+  "/home/turtlebot/catkin_ws/devel/include/openni_camera/OpenNIConfig.h"
+  "/home/turtlebot/catkin_ws/devel/share/openni_camera/docs/OpenNIConfig.dox"
+  "/home/turtlebot/catkin_ws/devel/share/openni_camera/docs/OpenNIConfig-usage.dox"
+  "/home/turtlebot/catkin_ws/devel/lib/python2.7/dist-packages/openni_camera/cfg/OpenNIConfig.py"
+  "/home/turtlebot/catkin_ws/devel/share/openni_camera/docs/OpenNIConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/openni_camera_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
